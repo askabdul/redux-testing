1af1ba262c58f7d37dd5adcb5b7db662c6b1aa64
salam aziz <azizsalam148@gmail.com>
2021-05-18 14:56:14 +0000

    setup

---
+++ src/components/TaskList.js
@@ -0,0 +1,19 @@
+import React, {useEffect} from "react";
+import Task from "./Task";
+const TaskList = (props) => {
+
+    useEffect(() => {
+        console.log("----", props);
+    })
+  return (
+    <div className="task-list">
+      <div className="task-list-title">
+        <strong>{props.status}</strong>
+      </div>
+      {props.tasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
+    </div>
+  );
+};
+export default TaskList;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import tasks from './reducers';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(tasks);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
